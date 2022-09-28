ba15e51ab5bf04117bcfe5cd2fa998f0a7d1f5e1
Nahuel Rudel <nahuelalerudel@gmail.com>
2022-09-27 22:01:50 -0300

    orden de compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,9 +2,28 @@ import "./styles.css"
 import { useCartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
 import ItemCart from "../ItemCart/ItemCart"
+import { addDoc, collection, getFirestore } from "firebase/firestore"
 
 const Cart = () => {
     const {cart, totalPrice} = useCartContext()
+
+    const order = {
+        buyer: {
+            name: "Nahuel",
+            email: "nahuel@gmail.com",
+            phone: "12345678",
+            adress: "direccion 1234"
+        },
+        items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore()
+        const ordersCollection = collection(db, "orders")
+        addDoc(ordersCollection, order)
+        .then(({id}) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -21,6 +40,7 @@ const Cart = () => {
                 cart.map(product => <ItemCart key={product.id} product={product} />)
             }
             <p>Total: ${totalPrice()}</p>
+            <button onClick={handleClick}>Generar orden de compra</button>
         </>
     )
 }
